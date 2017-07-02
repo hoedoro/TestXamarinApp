86fc4184a1a8e28717f76aac443aab7489205ac0
hoedoro <hoedoro@gmx.de>
2017-07-02 16:07:00 +0200

    add test app project

---
+++ TestXamarinApp/TestXamarinApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestXamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+        }
+    }
+}
